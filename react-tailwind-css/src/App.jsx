18ac59379c278ebc2922d30f971c57422cbe82fd
ahashmi5401 <ahashmi5401@gmail.com>
2025-12-23 21:47:24 +0500

    Add react-tailwind-css subfolder

---
+++ react-tailwind-css/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-300 h-screen w-full'>
+      <h1 className='text-5xl font-bold  text-center text-white pt-20 text-shadow-2xs'>
+        Hello,I'm using Tailwind CSS with Vite and React!
+      </h1>
+      <p className='text-center text-white pt-20'>
+        This is a simple example of a React application styled with Tailwind CSS
+        and bundled using Vite.
+      </p>
+
+
+    </div>
+  )
+}
+
+export default App
